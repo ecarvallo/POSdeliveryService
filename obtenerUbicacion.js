2598c6cb5c7d94c4325d6153223edd0dbd613340
ecarvallo <enriqueimmontemar@gmail.com>
2021-11-26 00:38:02 -0300

    Add files via upload

---
+++ obtenerUbicacion.js
@@ -0,0 +1,8 @@
+exports.handler = async (event) => {
+    // TODO implement
+    const response = {
+        statusCode: 200,
+        body: JSON.stringify('Tu ubicación es: -32.970855, -71.539264'),
+    };
+    return response;
+};
---
+++ postearNotificacion.js
@@ -0,0 +1,8 @@
+exports.handler = async (event) => {
+    // TODO implement
+    const response = {
+        statusCode: 200,
+        body: JSON.stringify('Tu pedido está en camino.'),
+    };
+    return response;
+};
